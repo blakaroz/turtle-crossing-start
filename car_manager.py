0c2d59ec8e61ab3bd2917c7744fee5ecebe0da4a
„blakaroz” <karolina.blazytko@gmail.com>
2023-12-04 23:53:01 +0100

    define player and move control, define random car

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,20 @@
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+y_place = random.randint(-230,250)
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("square")
+        self.color(random.choice(COLORS))
+        self.shapesize(stretch_wid=20/20, stretch_len=40/20)
+        self.goto(270,y_place)
+
+    def car_move(self):
+        new_x = self.xcor() + STARTING_MOVE_DISTANCE
+        self.goto(y_place, new_x)
--- player.py
+++ player.py
@@ -1,7 +1,21 @@
-STARTING_POSITION = (0, -280)
+from turtle import Turtle
+
+STARTING_POSITION = (0, -270)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("red")
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def go_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(0,new_y)
+
+    
