be1c8e9ae34f2fcdfed2fec0128f570b260a2e5f
„blakaroz” <karolina.blazytko@gmail.com>
2023-12-06 20:27:54 +0100

    no change

--- car_manager.py
+++ car_manager.py
@@ -3,18 +3,28 @@ import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
 y_place = random.randint(-230,250)
 
 class CarManager(Turtle):
+    cars = []
     def __init__(self):
         super().__init__()
         self.penup()
         self.shape("square")
         self.color(random.choice(COLORS))
         self.shapesize(stretch_wid=20/20, stretch_len=40/20)
+        y_place = random.randint(-230, 250)
         self.goto(270,y_place)
+        self.setheading(180)
+        CarManager.cars.append(self)
 
     def car_move(self):
-        new_x = self.xcor() + STARTING_MOVE_DISTANCE
-        self.goto(y_place, new_x)
+        self.forward(STARTING_MOVE_DISTANCE)
+
+
+    def car_remove(self):
+        # Check if the car is beyond the left edge of the screen
+        if self.xcor() < -300:
+            CarManager.cars.remove(self)
+            self.hideturtle()
+
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
-car_manager = CarManager()
+counter = 0
 
 screen.listen()
 screen.onkey(player.go_up,"Up")
@@ -18,6 +18,17 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # Generate a new car in each 6 iteration
+    if counter % 6 ==0:
+        new_car = CarManager()
+
+    # Move each car
+    for car in CarManager.cars:
+        car.car_move()
+        car.car_remove()
+
+    counter += 1
 
 
 screen.exitonclick()
